1818eb0ce40528bf5a361427406030319e6fa9b6
Saddab <saddabakhtar17@gmail.com>
2024-02-28 14:50:22 +0530

    add JokeHub File

---
+++ README.md
@@ -0,0 +1,2 @@
+# JokeHub
+### Introducing '**JokeHub**,' the ultimate Chrome extension for endless laughter! With **JokeHub**, your browser transforms into a haven of humor, delivering a daily dose of side-splitting jokes directly to your fingertips. Powered by **HTML, CSS, JavaScript**, and a curated **JSON** database, **JokeHub** ensures a seamless and delightful user experience. Install **JokeHub** now and let the laughter begin with just a click!
---
+++ app.js
@@ -0,0 +1,18 @@
+const getJokes = async () => {
+  try {
+    const res = await fetch(
+      "https://official-joke-api.appspot.com/jokes/random"
+    );
+    const data = await res.json();
+    const myjokes = document.querySelector("#myjoke");
+    // Construct the joke text using the setup and punchline
+    const jokeText = `${data.setup} ${data.punchline}`;
+    myjokes.innerHTML = jokeText;
+  } catch (error) {
+    console.error("Error fetching joke:", error);
+  }
+};
+
+window.addEventListener("load", () => {
+  getJokes();
+});
